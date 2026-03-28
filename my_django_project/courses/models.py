c7dea71776bb4acfbf9982edcc6b1bffde2ca304
EldarIsak <Isakzhanov186@bk.ru>
2026-03-28 13:25:32 +0500

    Homework 30.1

---
+++ my_django_project/courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    preview = models.ImageField(upload_to='courses/previews/', blank=True, null=True)
+    description = models.TextField()
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    preview = models.ImageField(upload_to='lessons/previews/', blank=True, null=True)
+    video_url = models.URLField()
