ce4f4c1ef6d8f12532e23bb2fdf0b486e5c110f5
EldarIsak <Isakzhanov186@bk.ru>
2026-04-02 18:31:18 +0500

    Homework 30.2

--- my_django_project/users/urls.py
+++ my_django_project/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, PaymentListView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('payments/', PaymentListView.as_view()),
 ]
--- my_django_project/users/views.py
+++ my_django_project/users/views.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
+from rest_framework import viewsets, generics, filters
+from .models import User, Payment
+from .serializers import UserSerializer, PaymentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    # Фильтрация по полям
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ['course', 'lesson', 'payment_method']
+    ordering_fields = ['date']
