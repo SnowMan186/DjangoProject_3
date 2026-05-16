91df1da6b2faa6b7ef536ace81454408389ad323
EldarIsak <Isakzhanov186@bk.ru>
2026-05-16 19:08:56 +0500

    Homework 35.2

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -1,5 +1,14 @@
-from .base import *
 import os
+import sys
+from pathlib import Path
+
+repo_root = Path(__file__).parent.parent.parent.resolve()
+sys.path.append(str(repo_root))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
+
+from config.settings.base import *
+
 
 DATABASES = {
     'default': {
@@ -7,10 +16,9 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'test_db'),
         'USER': os.environ.get('POSTGRES_USER', 'test_user'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'test_password'),
-        'HOST': 'localhost',
+        'HOST': 'localhost', # Сервис postgres доступен по имени хоста
         'PORT': '5432',
     }
 }
-
 
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'courses']
