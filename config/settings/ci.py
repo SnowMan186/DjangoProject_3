d9438ba20cc4b2f77d8b0f3b6a00f7dc02655591
EldarIsak <Isakzhanov186@bk.ru>
2026-05-16 19:02:43 +0500

    Homework 35.2

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -1,3 +1,16 @@
 from .base import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'test_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'test_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'test_password'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'courses']
